48a064179c098775b8d75cc5674e7879b60eebee
camilarivadeneyra <c.crivadeneyra@gmail.com>
2022-06-11 20:14:58 -0300

    css

--- js/main.js
+++ js/main.js
@@ -1,277 +1,148 @@
-
 // class Producto {
-//     constructor (nombre, precio, cantidad) {
+//     constructor(nombre, precio, stock) {
 //         this.nombre = nombre,
-//         this.precio = Number (precio),
-//         this.cantidad = cantidad
+//         this.precio = Number(precio),
+//         this.stock = stock
 //     }
-//     actualizarCantidad (stock) {
-//         this.cantidad = this.cantidad - stock
+//     actualizarStock(cantidad) {
+//         this.stock = this.stock - cantidad
 //     }
 // }
-
 
 // const arrayProductos = [];
 
-// arrayProductos.push (new Producto ("Cafe Colombia", 1550, 20));
-// arrayProductos.push (new Producto ("Cafe Etiopia", 1650, 10));
-// arrayProductos.push (new Producto ("Cafe Kenia", 1600, 10));
-// arrayProductos.push (new Producto ("Cafe Nicaragua", 1500, 10));
+// arrayProductos.push(new Producto("Cafe Colombia", 1550, 20));
+// arrayProductos.push(new Producto("Cafe Etiopia", 1650, 10));
+// arrayProductos.push(new Producto("Cafe Kenia", 1600, 10));
+// arrayProductos.push(new Producto("Cafe Nicaragua", 1500, 10));
 
 
-// const menorMayor = () => {
-//     arrayProductos.sort ((a,b,c,d) => a.precio - b.precio - c.precio - d.precio);
-//     verListaEnOrden ();
-// }
-
-// const mayorMenor = () => {
-//     arrayProductos.sort( (a,b,c,d) => d.precio - c.precio - b.precio - a.precio);
-//     verListaEnOrden();
+// const ordenarMenorMayor = () => {
+//     arrayProductos.sort( (a,b,c) => a.precio - b.precio - c.precio);
+//     mostrarListaOrdenada();
 // };
 
-// const verListaEnOrden = () => {
-//     arrayProductos.forEach (producto => arrayProductos.push (producto.nombre + "$" + producto.precio));
-//     alert ("LISTA DE PRECIOS: "+"\n"+array.join ("\n"));
-// }
+// const ordenarMayorMenor = () => {
+//     arrayProductos.sort( (a,b,c) => c.precio - b.precio - a.precio);
+//     mostrarListaOrdenada();
+// };
 
-// //compra
- 
+// const mostrarListaOrdenada = () => {
+//     const array = [];
+//     arrayProductos.forEach( producto => array.push(producto.nombre+" $"+producto.precio));
+//     alert("Lista de precios: "+"\n"+array.join("\n"));
+// };
+
+
+
 // let total = 0;
 
-// const agregarAlCarrito = () => {
+// const agregarProductoCarrito = () => {
 //     let otroProducto;
 //     let producto = "";
 //     let cantidad = 0;
 //     let precio = 0;
-
+//     let bienvenida;
 
 //     do {
-//         bievenida = prompt ("Bienvenidos a PUERTO BLEST. VIVI TU PROPIA EXPERIENCIA SENSORIAL!");
-//         producto = prompt ("¿En donde te gustaria sentirte? ¿Colombia, Etiopia, Guatemala, Kenia, Nicaragua o Peru?");
-//         cantidad = parseInt (prompt("¿Cuantos desea comprar?"));
+//         bienvenida = prompt ("Bienvenidos a PUERTO BLEST. VIVI TU PROPIA EXPERIENCIA SENSORIAL!");
+//         producto = prompt("Que sabor te gustaria sentir? Colombia, Etiopia o Kenia?");
+//         cantidad = parseInt(prompt("Que cantidad desea agregar?"));
 
-//        switch (producto) {
-//         case arrayProductos [0].nombre:
-//             arrayProductos [0].actualizarCantidad (stock);
-//                 if (arrayProductos[0].stock < 0  || isNaN (stock)) {
-//                     alert ("Lo sentimos! Este producto se agoto!");
+//         switch(producto) {
+//             case arrayProductos[0].nombre:
+//                 arrayProductos[0].actualizarStock(cantidad);
+//                 if (arrayProductos[0].stock < 0 || isNaN(cantidad)) {
+//                     alert("Lo sentimos. Este producto se agoto!");
 //                     precio = 0;
 //                     cantidad = 0;
-//                 } else  {
+//                 } else {
 //                     precio = arrayProductos[0].precio;
 //                 }
 //                 break;
-//         case arrayProductos [1].nombre:
-//             arrayProductos [1].actualizarCantidad (stock);
-//                 if (arrayProductos[1].stock < 0  || isNaN (stock)) {
-//                     alert ("Lo sentimos! Este producto se agoto!");
+//             case arrayProductos[1].nombre:
+//                 arrayProductos[1].actualizarStock(cantidad);
+//                 if (arrayProductos[1].stock < 0 || isNaN(cantidad)) {
+//                     alert("Lo sentimos. Este producto se agoto!");
 //                     precio = 0;
 //                     cantidad = 0;
 //                 } else {
-//                     precio = arrayProductos [1].precio;
+//                     precio = arrayProductos[1].precio;
 //                 }
 //                 break;
-//         case arrayProductos [2].nombre:
-//             arrayProductos [2].actualizarCantidad (stock);
-//                 if (arrayProductos[2].stock < 0  || isNaN (stock)) {
-//                     alert ("Lo sentimos! Este producto se agoto!");
+//             case arrayProductos[2].nombre:
+//                 arrayProductos[2].actualizarStock(cantidad);
+//                 if (arrayProductos[2].stock < 0 || isNaN(cantidad)) {
+//                     alert("Lo sentimos. Este producto se agoto!");
 //                     precio = 0;
 //                     cantidad = 0;
 //                 } else {
-//                     precio = arrayProductos [2].precio;
+//                     precio = arrayProductos[2].precio;
 //                 }
 //                 break;
-//         case arrayProductos [3].nombre:
-//             arrayProductos [3].actualizarCantidad (stock);
-//                 if (arrayProductos[3].stock < 0  || isNaN (stock)) {
-//                     alert ("Lo sentimos! Este producto se agoto!");
+//             case arrayProductos[2].nombre:
+//                 arrayProductos[2].actualizarStock(cantidad);
+//                 if (arrayProductos[2].stock < 0 || isNaN(cantidad)) {
+//                     alert("Lo sentimos. Este producto se agoto!");
 //                     precio = 0;
 //                     cantidad = 0;
 //                 } else {
-//                         precio = arrayProductos [3].precio;
+//                     precio = arrayProductos[2].precio;
 //                 }
-//                     break;
-//         default:
-//             alert ("Lo ingresado no es valido.");
-//             precio = 0;
-//             cantidad = 0;
-//     }
+//                 break;
+//             default:
+//                 alert("El campo ingresado no es valido.");
+//                 precio = 0;
+//                 cantidad = 0;
+//         }
 
-//     total += precio * cantidad;
-//     otroProducto = confirm ("Desea agregar algun otro producto?");   
-// } while (otroProducto);
+//         total = total + precio * cantidad;
+//         otroProducto = confirm("Desea agregar algun otro producto?");
+//     } while (otroProducto);
 // };
 
 
-// const precioEnvio = (total) => {
-//     let confirmarla = confirm ("Lo enviamos a tu domicilio?");
-//     if (confirmarla && total >= 3000) {
-//         alert ("ENVIO GRATIS! El total de tu compra es "+total);
-//     } else if (confirmarla && total < 3000 && total != 0) {
-//         total = total +600;
-//     } else {
-//         alert ("EL TOTAL DE SU COMPRA ES "+total);
-//     } 
+
+// const obtenerDescuento = (total) => {
+//     if ( total >= 6500 ) {
+//         total = total * 0.8;
+//         alert(" TENES UN 15% DE DESCUENTO EN TU COMPRA");
+//     }
 //     return total;
-// }; 
-
-
-// let finalizarCompra = confirm ("EL TOTAL DE SU COMPRA ES: "+total+"\n\n¿Desea finalizar su compra?")
-
-// if (finalizarCompra) {
-//     alert ("Gracias por su compra :)")
-// } else  {
-//     alert ("Gracias por comprar en PuertoBlest. Te esperamos pronto!")
-// }
+// };
 
 
 
-
-class Producto {
-    constructor(nombre, precio, stock) {
-        this.nombre = nombre,
-        this.precio = Number(precio),
-        this.stock = stock
-    }
-    actualizarStock(cantidad) {
-        this.stock = this.stock - cantidad
-    }
-}
-
-const arrayProductos = [];
-
-arrayProductos.push(new Producto("Cafe Colombia", 1550, 20));
-arrayProductos.push(new Producto("Cafe Etiopia", 1650, 10));
-arrayProductos.push(new Producto("Cafe Kenia", 1600, 10));
-arrayProductos.push(new Producto("Cafe Nicaragua", 1500, 10));
-
-
-const ordenarMenorMayor = () => {
-    arrayProductos.sort( (a,b,c) => a.precio - b.precio - c.precio);
-    mostrarListaOrdenada();
-};
-
-const ordenarMayorMenor = () => {
-    arrayProductos.sort( (a,b,c) => c.precio - b.precio - a.precio);
-    mostrarListaOrdenada();
-};
-
-const mostrarListaOrdenada = () => {
-    const array = [];
-    arrayProductos.forEach( producto => array.push(producto.nombre+" $"+producto.precio));
-    alert("Lista de precios: "+"\n"+array.join("\n"));
-};
+// const precioDeEnvio = (total) => {
+//     let confirmacion = confirm("Lo enviamos a tu domicilio?");
+//     if ( confirmacion && total >= 3000 ) {
+//         alert("NEVIOS GRATIS, el total de tu compra es de "+total);
+//     } else if ( confirmacion && total < 3000 && total != 0 ) {
+//         total = total + 680;
+//     } else {
+//         alert("El total de tu compra es de "+total);
+//     }
+//     return total;
+// };
 
 
 
-let total = 0;
-
-const agregarProductoCarrito = () => {
-    let otroProducto;
-    let producto = "";
-    let cantidad = 0;
-    let precio = 0;
-    let bienvenida;
-
-    do {
-        bienvenida = prompt ("Bienvenidos a PUERTO BLEST. VIVI TU PROPIA EXPERIENCIA SENSORIAL!");
-        producto = prompt("Que sabor te gustaria sentir? Colombia, Etiopia o Kenia?");
-        cantidad = parseInt(prompt("Que cantidad desea agregar?"));
-
-        switch(producto) {
-            case arrayProductos[0].nombre:
-                arrayProductos[0].actualizarStock(cantidad);
-                if (arrayProductos[0].stock < 0 || isNaN(cantidad)) {
-                    alert("Lo sentimos. Este producto se agoto!");
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    precio = arrayProductos[0].precio;
-                }
-                break;
-            case arrayProductos[1].nombre:
-                arrayProductos[1].actualizarStock(cantidad);
-                if (arrayProductos[1].stock < 0 || isNaN(cantidad)) {
-                    alert("Lo sentimos. Este producto se agoto!");
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    precio = arrayProductos[1].precio;
-                }
-                break;
-            case arrayProductos[2].nombre:
-                arrayProductos[2].actualizarStock(cantidad);
-                if (arrayProductos[2].stock < 0 || isNaN(cantidad)) {
-                    alert("Lo sentimos. Este producto se agoto!");
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    precio = arrayProductos[2].precio;
-                }
-                break;
-            case arrayProductos[2].nombre:
-                arrayProductos[2].actualizarStock(cantidad);
-                if (arrayProductos[2].stock < 0 || isNaN(cantidad)) {
-                    alert("Lo sentimos. Este producto se agoto!");
-                    precio = 0;
-                    cantidad = 0;
-                } else {
-                    precio = arrayProductos[2].precio;
-                }
-                break;
-            default:
-                alert("El campo ingresado no es valido.");
-                precio = 0;
-                cantidad = 0;
-        }
-
-        total = total + precio * cantidad;
-        otroProducto = confirm("Desea agregar algun otro producto?");
-    } while (otroProducto);
-};
+// const obtenerTotal = (total, envio) => {
+//     console.log(total);
+//     console.log(envio)
+//     total = total + envio;
+//     alert("EL TOTAL DE SU COMPRA ES "+total);
+// };
 
 
+// const comprarProductos = () => {
+//     if ( confirm("Queres ver los productos de menor a mayor precio?")) {
+//         ordenarMenorMayor();
+//     } else {
+//         ordenarMayorMenor();
+//     }
+//     agregarProductoCarrito();
+//     obtenerTotal(precioDeEnvio(obtenerDescuento(total)));
+// };
 
-const obtenerDescuento = (total) => {
-    if ( total >= 6500 ) {
-        total = total * 0.8;
-        alert(" TENES UN 15% DE DESCUENTO EN TU COMPRA");
-    }
-    return total;
-};
-
-
-
-const precioDeEnvio = (total) => {
-    let confirmacion = confirm("Lo enviamos a tu domicilio?");
-    if ( confirmacion && total >= 3000 ) {
-        alert("NEVIOS GRATIS, el total de tu compra es de "+total);
-    } else if ( confirmacion && total < 3000 && total != 0 ) {
-        total = total + 680;
-    } else {
-        alert("El total de tu compra es de "+total);
-    }
-    return total;
-};
-
-
-
-const obtenerTotal = (total, envio) => {
-    console.log(total);
-    console.log(envio)
-    total = total + envio;
-    alert("EL TOTAL DE SU COMPRA ES "+total);
-};
-
-
-const comprarProductos = () => {
-    if ( confirm("Queres ver los productos de menor a mayor precio?")) {
-        ordenarMenorMayor();
-    } else {
-        ordenarMayorMenor();
-    }
-    agregarProductoCarrito();
-    obtenerTotal(precioDeEnvio(obtenerDescuento(total)));
-};
-
-comprarProductos();
+// comprarProductos();
