ea4d8cdc7425371371769e9c517d3c78b6fd3c97
camilarivadeneyra <c.crivadeneyra@gmail.com>
2022-07-17 13:13:40 -0300

    details

--- js/main.js
+++ js/main.js
@@ -231,6 +231,57 @@
 // })()
 
 
+/*LIBRARIES*/
+
+function swalCheckout () {
+  const swal = Swal.mixin({
+      customClass: {
+        confirmButton: 'boton__confirmar',
+        cancelButton: 'boton__cancelar'
+      },
+      buttonsStyling: false
+    })
+    
+    swal.fire({
+      title: 'Confirmar compra?',
+      html: 
+      `<form>
+      <input type="text" class="input__swal" name="nombre" placeholder="Nombre"><br>
+      <input type="text" class="input__swal" name="email" placeholder="Email"><br>
+      </form>`,
+      showCancelButton: true,
+      confirmButtonText: 'Comprar!',
+      cancelButtonText: 'Cancelar',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+          swal.fire(
+          'Hecho!',
+          'Gracias por confiar en ILON OUTDOORS',
+          'success')
+      }
+    
+    
+  });
+  }
+  
+  function toastAdd () {
+      Toastify({
+          text: "Agregado al carrito",
+          duration: 1000,
+          gravity: "top", 
+          position: "right", 
+          style: {
+          background: "#33691E",
+          }
+        }).showToast();
+  }
+
+
+
+
+
+
 
 /*SWEET ALERT */
 
